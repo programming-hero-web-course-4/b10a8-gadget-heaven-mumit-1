4ac7c28b4669629e30a8442fdcc3184b24da79a3
mumit-1 <mumitkhan85@gmail.com>
2024-12-05 01:12:15 +0600

    ok2

--- src/Comp/SingleDetsil/SingleDetail.jsx
+++ src/Comp/SingleDetsil/SingleDetail.jsx
@@ -4,7 +4,7 @@ const SingleDetail = () => {
   const { product_id } = useParams();
   const data = useLoaderData();
   const newData = data.find((gadget) => gadget.product_id === product_id);
-  const {product_title,product_image,price} = newData;
+  const {product_title,product_image,price,availability,description,specification} = newData;
   return (
     <div>
       <div className="hero bg-base-200 min-h-screen">
@@ -18,6 +18,20 @@ const SingleDetail = () => {
             <p className="py-6">
               {price}
             </p>
+            <div>
+              {availability?(<button className=" border rounded-xl px-1 border-green-600 bg-green-300">in stock</button>) : (<button className="border rounded-xl px-1 border-red-600 bg-red-300">Out of stock</button>)}
+            </div>
+            <div><p>{description}</p></div>
+            <div>
+              <p>Specification:</p>
+              <ul className="list-decimal">
+                {specification.map(info=><li>{info}</li>)}
+              </ul>
+            </div>
+            <div>
+              <p>Rating:</p>
+              
+            </div>
             <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
